e794bd7726351eeca95b1b308f40db470a3cf64a
Denis Fetinin <denis.fetinin@lamoda.ru>
2023-03-22 14:57:29 +0700

    add votes statistics

--- src/routes/room/[slug]/+page.svelte
+++ src/routes/room/[slug]/+page.svelte
@@ -91,6 +91,60 @@
 		await unsubVoters();
 		await unsubTasks();
 	});
+
+	function minVote(voters: import('./proxy+page.server').Voter[]): number {
+		const votes = voters.map((v) => v.vote).filter(notUndefined);
+		if (!votes.length) return 0;
+
+		return Math.min(...votes);
+	}
+
+	function maxVote(voters: import('./proxy+page.server').Voter[]): number {
+		const votes = voters.map((v) => v.vote).filter(notUndefined);
+		if (!votes.length) return 0;
+
+		return Math.max(...votes);
+	}
+
+	function calStdDeviation(voters: import('./proxy+page.server').Voter[]): number {
+		const votes = voters.map((v) => v.vote).filter(notUndefined);
+		if (!votes.length) return 0;
+
+		// For the given votes of 3, 8, 10, 3:
+		// 1. Calculate the mean: `(3 + 8 + 10 + 3) / 4 = 6`
+		const mean = votes.reduce((p, c) => p + c) / votes.length;
+		console.log('std deviation', 'mean', mean);
+
+		// 2. Calculate the variance: `((3-6)² + (8-6)² + (10-6)² + (3-6)²) / 4 = 10`
+		const variance = votes.map((v) => Math.pow(v - mean, 2)).reduce((p, n) => p + n) / votes.length;
+		console.log('std deviation', 'variance', variance);
+
+		// 3. Calculate the standard deviation: `sqrt(10) ≈ 3.16`
+		return Math.sqrt(variance);
+	}
+
+	function calcCoefficientOfVariation(voters: import('./proxy+page.server').Voter[]): number {
+		const votes = voters.map((v) => v.vote).filter(notUndefined);
+		if (!votes.length) return 0;
+
+		// So, for the given votes of 3, 8, 10, 3:
+
+		// 1. Calculate the mean: `(3 + 8 + 10 + 3) / 4 = 6`
+		const mean = votes.reduce((p, c) => p + c) / votes.length;
+		console.log('CoefficientOfVariation', 'mean', mean);
+
+		// 2. Calculate the standard deviation: `sqrt(10) ≈ 3.16`
+		const variance = votes.map((v) => Math.pow(v - mean, 2)).reduce((p, n) => p + n) / votes.length;
+		const stdDeviation = Math.sqrt(variance);
+		console.log('CoefficientOfVariation', 'variance', variance);
+
+		// 3. Calculate the coefficient of variation: `3.16 / 6 ≈ 0.53`
+		return stdDeviation / mean;
+	}
+
+	function notUndefined<T>(x: T | undefined): x is T {
+		return x !== undefined;
+	}
 </script>
 
 <div class="columns">
@@ -226,6 +280,20 @@
 		{/if}
 	</div>
 </div>
+
+{#if currentTask?.vote}
+	<div class="columns is-centered">
+		<div class="column is-half">
+			<div class="box">
+				<p class="is-size-5">Votes statistics:</p>
+				<!-- votes: {#each voters as v (v.id)}{v.vote}{/each} -->
+				min, max: {minVote(voters)}, {maxVote(voters)}<br />
+				Std. deviation: {calStdDeviation(voters)}<br />
+				Coefficient of variation: {calcCoefficientOfVariation(voters)}
+			</div>
+		</div>
+	</div>
+{/if}
 
 <div class="columns is-centered">
 	<div class="column is-half">
