8beb999a114e721eb77e4b23399b948e0858b38a
Denis Fetinin <denis.fetinin@lamoda.ru>
2023-04-17 17:38:16 +0700

    update sentry, setup source upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { sentrySvelteKit } from '@sentry/sveltekit';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
+	plugins: [
+		sentrySvelteKit({
+      sourceMapsUploadOptions: {
+        org: 'Pocket Planner',
+        project: 'javascript-svelte',
+        authToken: 'https://9499d6f0d9204e50ab3e46b133a53e4f@o4504547848224769.ingest.sentry.io/4504818857869312',
+      },
+    }),
+		sveltekit()
+	],
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
