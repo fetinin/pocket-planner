42ce04370bc4690acde2046e9ad6e23c9eff7e06
Denis Fetinin <denis.fetinin@lamoda.ru>
2023-04-18 12:04:49 +0700

    temporary disable source upload

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { sentrySvelteKit } from '@sentry/sveltekit';
+// import { sentrySvelteKit } from '@sentry/sveltekit';
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [
-		sentrySvelteKit({
-      sourceMapsUploadOptions: {
-        org: 'Pocket Planner',
-        project: 'javascript-svelte',
-        authToken: 'https://9499d6f0d9204e50ab3e46b133a53e4f@o4504547848224769.ingest.sentry.io/4504818857869312',
-      },
-    }),
+		// sentrySvelteKit({
+    //   sourceMapsUploadOptions: {
+    //     org: 'Pocket Planner',
+    //     project: '4504818857869312',
+    //     authToken: 'xxx',
+    //   },
+    // }),
 		sveltekit()
 	],
 
